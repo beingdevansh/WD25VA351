688bf0c0426dc8ceadd8a2b882f59f465ba1eaa2
beingdevansh <itsdevansh16@gmail.com>
2026-09-09 10:17:47 +0530

    New topics

---
+++ function.js
@@ -0,0 +1,9 @@
+function multiply(a, b) {
+    return a * b;
+}
+
+let x = Number(prompt("Enter 1st number :- "));
+let y = Number(prompt("Enter 2nd number :- "));
+
+let result = multiply(x, y);
+document.write("The result is = " + result + "<br>");
